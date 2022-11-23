ba68a4fdb4378153fcf57ba357e2f5f6fe7b6b90
Ajarek <ajarek@poczta.onet.pl>
2022-11-24 00:35:04 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  const url='https://hn.algolia.com/api/v1/search?query=pizza&page=0'
+
+  return (
+    <div className="App">
+     <h1> Search Hacker News</h1>
+    </div>
+  )
+}
+
+export default App
